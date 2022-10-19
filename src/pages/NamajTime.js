98e782c0a355ee62c9424f3a924fa333f0137e0d
Srejon Khan <srejonkhan79@gmail.com>
2022-10-19 12:07:56 +0600

    add prayer timings in namajtime

--- src/pages/NamajTime.js
+++ src/pages/NamajTime.js
@@ -3,15 +3,13 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const NamajTime = () => {
-  const [fajrTime, setFajrTime] = useState("");
+  const [timings, setTimings] = useState();
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
     } else {
-      alert(
-        "It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it."
-      );
+      alert("আপনার ডিভাইসটি জিও-লোকেশন সাপোর্ট করছে না, যা আমাদের সার্ভিসের জন্য প্রযোজ্য।");
     }
   }, []);
 
@@ -19,11 +17,11 @@ const NamajTime = () => {
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
 
-    fetch(
-      `https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${long}&method=1&school=1`
-    )
+    fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${long}&method=1&school=1`)
       .then((res) => res.json())
-      .then((json) => setFajrTime(json.data.timings.Fajr));
+      .then((json) => {
+        setTimings(json.data.timings);
+      });
   }
 
   function geoError(error) {
@@ -44,13 +42,56 @@ const NamajTime = () => {
         break;
     }
   }
+
+  function get12hTimeStr(oldTimeStr) {
+    return new Date("1970-01-01T" + oldTimeStr + "Z").toLocaleTimeString("en-US", {
+      timeZone: "UTC",
+      hour12: true,
+      hour: "numeric",
+      minute: "numeric",
+    });
+  }
+
   return (
     <div>
-      {/* {fajrTime && <h1>{fajrTime}</h1>} */}
       <h2 className="text-center">নামাজের সময়</h2>
-      <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
-        <h2>ফজর</h2> <h2>{fajrTime}</h2>
-      </div>
+      {timings && (
+        <div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>ফজর</h2> <h2>{get12hTimeStr(timings.Fajr)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>যোহর</h2> <h2>{get12hTimeStr(timings.Dhuhr)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>আসর</h2> <h2>{get12hTimeStr(timings.Asr)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>মাগরিব</h2> <h2>{get12hTimeStr(timings.Maghrib)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>এশা</h2> <h2>{get12hTimeStr(timings.Isha)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>সূর্যোদয়</h2> <h2>{get12hTimeStr(timings.Sunrise)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>সূর্যাস্ত</h2> <h2>{get12hTimeStr(timings.Sunset)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>রাতের প্রথম তৃতীয়ার্ধ</h2> <h2>{get12hTimeStr(timings.Firstthird)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>মধ্যরাত</h2> <h2>{get12hTimeStr(timings.Midnight)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>রাতের শেষ তৃতীয়ার্ধ</h2> <h2>{get12hTimeStr(timings.Lastthird)}</h2>
+          </div>
+          <div className="flex items-center justify-evenly bg-gray-200 text-black rounded-md">
+            <h2>সেহরির শেষ সময়</h2> <h2>{get12hTimeStr(timings.Imsak)}</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
